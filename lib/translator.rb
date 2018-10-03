573b31ce1daf6ccc828ad6fd3b2bc38f2df1f676
Iman Jawad <imanj12@gmail.com>
2018-10-03 00:41:38 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 def load_library(path)
   library = { "get_emoticon" => {}, "get_meaning" => {} }
   YAML.load_file(path).each do |meaning, emoticons|
-    
+    library["get_meaning"][]
   end
   library
 end

bd8ff80be617f4afe2ebf337878be2303f16216b
Iman Jawad <imanj12@gmail.com>
2018-10-03 00:58:52 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,9 @@ end
 
 def get_japanese_emoticon(path,emoticon)
   library = load_library(path)
-  library["get_emoticon"].each do |eng, jap|
+  library["get_emoticon"].each do |english, japanese|
+    
+  end
 end
 
 def get_english_meaning

1ecdef180b69a45856bc46398b5cea64a3e3e736
Iman Jawad <imanj12@gmail.com>
2018-10-03 00:46:15 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,8 @@ def load_library(path)
   YAML.load_file(path).each do |meaning, emoticons|
     english_emoticon = emoticons[0]
     japanese_emoticon = emoticons[1]
-    library["get_meaning"][japanese_emoticon]
+    library["get_meaning"][japanese_emoticon] = meaning
+    
     
   end
   library
